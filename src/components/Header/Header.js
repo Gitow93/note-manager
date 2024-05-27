de76d17666d7e2359d95a2006f1bf9f79ebd0301
Jorge Jiménez Muñoz <jjimenezmunoz@paradigmadigital.com>
2024-05-27 13:22:46 +0200

    change Button and Header funcionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import "./header.css";
 
-const Header = () => (
-  <header className="header">
-    <Logo />
-    <Button href="/create-note">New Note +</Button>
-  </header>
-);
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/create-note");
+  };
+
+  return (
+    <header className="header">
+      <Logo />
+      <Button onClick={handleClick}>New Note +</Button>
+    </header>
+  );
+};
 
 export default Header;
