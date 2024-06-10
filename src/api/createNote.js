d3e1a1f7f0e56ddf1c440ee2e334041eb89c3f7d
Jorge Jiménez Muñoz <jjimenezmunoz@paradigmadigital.com>
2024-06-10 11:43:40 +0200

    add new attribute to create note api call

--- src/api/createNote.js
+++ src/api/createNote.js
@@ -3,7 +3,11 @@ import { addNoteSuccess } from "../redux/notesSlice";
 
 export const createNote = (note) => async (dispatch) => {
   try {
-    const response = await axios.post("http://localhost:3200/notes", note);
+    const newNote = {
+      ...note,
+      created_at: new Date().toLocaleDateString(),
+    };
+    const response = await axios.post("http://localhost:3200/notes", newNote);
     dispatch(addNoteSuccess(response.data));
   } catch (error) {
     console.error("Failed to create note:", error);
