7d165a1ea38279ce45b062d22e34447d0b01af43
Jorge Jiménez Muñoz <jjimenezmunoz@paradigmadigital.com>
2024-06-10 11:41:17 +0200

    add some changes to form.js and include translations on create note page

--- src/components/CreateNoteForm/Form.js
+++ src/components/CreateNoteForm/Form.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createNote } from "../../api/createNote";
 import { useTranslation } from "react-i18next";
+import "./Form.css";
 
 const CreateNoteForm = () => {
   const { t } = useTranslation();
@@ -15,9 +16,9 @@ const CreateNoteForm = () => {
     const value = e.target.value;
     setTitle(value);
     if (value.length < 3) {
-      setTitleError(t("title_min_length"));
+      setTitleError(t("form.title_min_length"));
     } else if (value.length > 20) {
-      setTitleError(t("title_max_length"));
+      setTitleError(t("form.title_max_length"));
     } else {
       setTitleError("");
     }
@@ -27,7 +28,7 @@ const CreateNoteForm = () => {
     const value = e.target.value;
     setContent(value);
     if (value.length < 3) {
-      setContentError(t("content_min_length"));
+      setContentError(t("form.content_min_length"));
     } else {
       setContentError("");
     }
@@ -43,18 +44,28 @@ const CreateNoteForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label>{t("title")}</label>
-        <input type="text" value={title} onChange={handleTitleChange} />
+      <div className="title-container">
+        <label>{t("form.title")}</label>
+        <input
+          className="title-input"
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+        />
         {titleError && <p>{titleError}</p>}
       </div>
-      <div>
-        <label>{t("content")}</label>
-        <textarea value={content} onChange={handleContentChange} rows="5" />
+      <div className="content-container">
+        <label>{t("form.content")}</label>
+        <textarea
+          className="textarea-container"
+          value={content}
+          onChange={handleContentChange}
+          rows="5"
+        />
         {contentError && <p>{contentError}</p>}
       </div>
       <button type="submit" disabled={titleError || contentError}>
-        {t("save_note")}
+        {t("form.save_note")}
       </button>
     </form>
   );
--- src/pages/CreateNote.js
+++ src/pages/CreateNote.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Form from "../components/CreateNoteForm/Form";
+import { useTranslation } from "react-i18next";
+import "./CreateNote.css";
 
 const CreateNote = () => {
+  const { t } = useTranslation();
   return (
-    <div>
-      <h1>Create Note</h1>
+    <div className="form-container">
+      <h1>{t("form.heading")}</h1>
       <Form />
     </div>
   );
