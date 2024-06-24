9519048e00a003f074670c800fcf59c6954d5358
Jorge Jiménez Muñoz <jjimenezmunoz@paradigmadigital.com>
2024-06-24 11:10:13 +0200

    add new translations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,21 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchNotes } from "../api/getNotes";
 import NotesList from "../components/Notes/NotesList";
+import "./Home.css";
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
   const dispatch = useDispatch();
   const notes = useSelector((state) => state.notes.allNotes);
+  const { t } = useTranslation();
 
   useEffect(() => {
     dispatch(fetchNotes());
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Notes</h1>
+    <div className="home-layout">
+      <h1>{t("home.title")}</h1>
       <NotesList notes={notes} />
     </div>
   );
